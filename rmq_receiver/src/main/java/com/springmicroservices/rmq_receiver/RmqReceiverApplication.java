bb88ae29acf7af4771e0f2523ff48c9cfcea544e
Milosz Pankanin <m.pankanin@be-tse.com>
2024-11-06 16:41:07 +0100

    Added beans to convert rabbit messages to JSON

--- rmq_receiver/src/main/java/com/springmicroservices/rmq_receiver/RmqReceiverApplication.java
+++ rmq_receiver/src/main/java/com/springmicroservices/rmq_receiver/RmqReceiverApplication.java
@@ -1,13 +1,21 @@
 package com.springmicroservices.rmq_receiver;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RmqReceiverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RmqReceiverApplication.class, args);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
